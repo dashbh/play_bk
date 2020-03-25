ae3b40a6f3233a7e863db758f95cc22c1b1279c3
Bhabani Prasad Dash <bhabani.dash@outlook.com>
2020-03-26 04:22:53 +0530

    Setup AirBnB EsLint

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
 
-const App = () => {
-  return (
-    <h2>
-      Welcome to Play !! #4
-    </h2>
-  );
-}
+const App = () => (
+  <h2>
+    Welcome to Play !
+  </h2>
+);
 
 export default App;
